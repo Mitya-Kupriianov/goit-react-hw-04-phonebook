39a4bdcab27b209a3fe6a676b00f0c6e325e5195
Mitya-Kupriianov <MityaBlow@gmail.com>
2022-09-21 23:44:14 +0300

    task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,53 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    if (contacts) this.setState({ contacts: JSON.parse(contacts) });
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== this.prevState) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  handleSubmit = newContact => {
-    const { contacts } = this.state;
-    const findSubmit = contacts.find(
-      ({ name }) => name.toLowerCase() === newContact.name.toLowerCase()
-    );
-    if (findSubmit) {
+export function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleSubmit = newContact => {
+    if (
+      contacts.find(
+        ({ name }) => name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    )
       return alert(`${newContact.name} is already in contacts.`);
-    }
-    this.setState({ contacts: [newContact, ...contacts] });
+
+    setContacts([newContact, ...contacts]);
   };
 
-  eventFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const eventFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  idDelete = id => {
-    this.setState({
-      contacts: this.state.contacts.filter(contact => contact.id !== id),
-    });
+  const idDelete = e => {
+    setContacts(prevContacts => prevContacts.filter(({ id }) => id !== e));
   };
 
-  render() {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.handleSubmit} />
-        <h2>Contacts</h2>
-        <Filter filter={this.state.filter} eventFilter={this.eventFilter} />
-        <ContactList
-          filter={this.state.filter}
-          contacts={this.state.contacts}
-          idDelete={this.idDelete}
-        />
-      </div>
-    );
-  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} eventFilter={eventFilter} />
+      <ContactList filter={filter} contacts={contacts} idDelete={idDelete} />
+    </div>
+  );
 }
